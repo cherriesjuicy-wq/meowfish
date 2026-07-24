f43e1db7ef04a1300e2beca48380c18041abb33f
cherriesjuicy-wq <hoangthule6104@gmail.com>
2026-07-24 08:36:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './', // 👈 BÀ THÊM ĐÚNG DÒNG NÀY VÀO ĐÂY NHA!
+    base: './', //
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
